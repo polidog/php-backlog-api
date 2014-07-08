28c3a984cbdc23650c0c4152f49cc63b16d46500
polidog <polidogs@gmail.com>
2014-07-08 20:59:32 +0900

    API用のインターフェースを用意

---
+++ lib/Backlog/Api/ApiInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryota_mc
+ * Date: 2014/07/08
+ * Time: 20:57
+ */
+
+namespace Backlog\Api;
+
+/**
+ * API用のインターフェース
+ * @package Backlog\Api
+ */
+interface ApiInterface
+{
+    public function get($path, array $parameters = [], $requestHeaders = []);
+
+    public function post($path, array $parameters = [], $requestHeaders = []);
+
+    public function put($path, array $parameters = [], $requestHeaders = []);
+
+    public function delete($path, array $parameters = [], $requestHeaders = []);
+}
