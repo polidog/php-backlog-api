08b5914d5e860eb8940ed98a88678186ec9cbafc
polidog <polidogs@gmail.com>
2014-07-08 20:44:30 +0900

    APIクライアント用のインタフェースを用意

---
+++ lib/Backlog/ClientInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryota_mc
+ * Date: 2014/07/08
+ * Time: 20:40
+ */
+
+interface ClientInterface {
+
+    /**
+     * APIを実行する
+     * @param $name
+     * @return mixed
+     */
+    public function api($name);
+
+    /**
+     * 認証を行う
+     * @param $apiKey
+     * @return mixed
+     */
+    public function authenticate($apiKey);
+}
