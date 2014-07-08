da7287643428d0525bdb8fcd0418a21a7fc0693a
polidog <polidogs@gmail.com>
2014-07-08 20:44:52 +0900

    APIクライアントを用意

---
+++ lib/Backlog/Client.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryota_mc
+ * Date: 2014/07/08
+ * Time: 20:39
+ */
+
+/**
+ * API用クライアントクラス
+ * Class Client
+ */
+class Client implements ClientInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function api($name)
+    {
+        return;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function authenticate($apiKey)
+    {
+        return;
+    }
+}
